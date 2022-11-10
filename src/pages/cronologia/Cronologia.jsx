4e6ae0e8ad501e3097b049b9f83eefa4602923a8
yayo.262002 <carlosmarchan2602@gmail.com>
2022-11-10 20:18:34 +0100

    carlos

--- src/pages/cronologia/Cronologia.jsx
+++ src/pages/cronologia/Cronologia.jsx
@@ -2,10 +2,16 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./Cronologia.scss";
 import { Link } from "react-router-dom";
+import imagen from '../../assets/i.png'
+import image from '../../assets/2.png'
+import SimpleBarReact from 'simplebar-react'
+import 'simplebar-react/dist/simplebar.min.css';
+
 
 export default function Cronologia() {
     const [personajes, setPersonajes] = useState([]);
     const [orden, setOrden] = useState(true);
+
    
     useEffect(() => {
         const getData = async () => {
@@ -25,17 +31,31 @@ export default function Cronologia() {
         }
     }, [orden, personajes]);
 
+   
     return (
+        
+
 <div className="all">
          <div className="head">
             <Link to="/"><h6 className='links-home'>HOME</h6></Link>
            </div>
 
-      <div className="Rectangle-4">
-           <button className="Ellipse-1" type="button" onClick={cambioOrden}> 0 </button>
-       </div>
+           <div className="Rectangle-4">
+           <button className="Ellipse-1" type="button" onClick={cambioOrden}><img src={imagen}/></button>
 
-        <div className="CRONOLOGIA">
+           <div className="Ellipse-2">    
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           <img src={image} className={orden ? 'bb' : 'cc'}/>
+           </div>
+       </div>
+       
+       
+        
+        <div className="CRONOLOGIA"><SimpleBarReact style={{maxHeight:500}}>
              {personajes.map((item, index) => (
                 <div key={index} className={index % 2 === 0 ? "izquierda" : "derecha"}>
                       <div className="card">
@@ -43,14 +63,18 @@ export default function Cronologia() {
                             <h5>{item.name}</h5>
                             <img  className="image-19" alt="" src={item.image} />
                       </div>
-                </div> ))}    
+                </div> ))}  </SimpleBarReact>  
            </div>
+           
+           
                   <div className="footer">
                         <Link to="/characters"><h6 className='links-footer'>PERSONAJES</h6> </Link> 
                         <Link to="/houses"><h6 className='links-footer'>CASAS</h6></Link>
                         <Link to="/chronology"><h6 className='links-footer'>CRONOLOGÍA</h6></Link>
                   </div>
+
 </div>
+
     );
     function cambioOrden() {
         setOrden(!orden);
@@ -67,6 +91,7 @@ export default function Cronologia() {
         }
         return personajes;
     }
+  
 }
 
 
